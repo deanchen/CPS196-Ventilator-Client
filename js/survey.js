f530f7abb9ddda45051453f95675cc329b4099fc
Dean Chen <dean@ocirs.com>
2011-03-23 16:59:39 -0400

    Working survey.

--- js/survey.js
+++ js/survey.js
@@ -1,130 +1,125 @@
 /**
  * @author Dean Chen
  */
-Ext.setup({
-    tabletStartupScreen: 'tablet_startup.png',
-    phoneStartupScreen: 'phone_startup.png',
-    icon: 'icon.png',
-    glossOnIcon: false,
-    onReady: function() {
-        var form = createForm();
-        new Ext.Panel({
-            fullscreen: true,
-            
-            layout: {
-                type: 'vbox',
-                align: 'stretch'
-            },
-            defaults: {
-                flex: 1,
-                id: 'education'
-            },
-            items: [form],
-        });
-    }
-});
+ var session_id;
+ var survey;
+	Ext.setup({
+	    tabletStartupScreen: 'tablet_startup.png',
+	    phoneStartupScreen: 'phone_startup.png',
+	    icon: 'icon.png',
+	    glossOnIcon: false,
+	    onReady: function() {
+	    	session_id = Ext.getDom('session_id').innerHTML;
+	    	console.log(session_id);
+	        var form = createForm();
+	        
+	    }
+	});
+
+ 
+var form;
+
 
 function createForm() {
-	var session_id = Ext.getDom('session_id').innerHTML;
-	var survey;
+	
 	$.ajax({
 		url: "http://vs.ocirs.com/rest/survey/groups?callback=?",
 		dataType: "jsonp",
 		success: processGroups
 	});
-	
-	function processGroups(data) {
-		survey = data;
-		for (var i = 0; i < data.length; i++) {
-			storeQuestions(data[i], i);
-		}
-	};
-	
-	function storeQuestions(group, index) {
-		$.ajax({
-			url: "http://vs.ocirs.com/rest/survey/questions/" + group.id +
-			"/" + session_id,
-			dataType: "jsonp",
-			success: function(data) {
-				survey[index].questions = data;
-				console.log(data);
-				var test = true;
-				for (var i = 0; i < survey.length; i++) {
-					if (survey[i].questions === undefined) {
-						test = false;
-					}
-				}
-				if (test) {
-					constructForm();
+}	
+function processGroups(data) {
+	survey = data;
+	for (var i = 0; i < data.length; i++) {
+		storeQuestions(data[i], i);
+	}
+};
+
+function storeQuestions(group, index) {
+	$.ajax({
+		url: "http://vs.ocirs.com/rest/survey/questions/" + group.id +
+		"/" + session_id,
+		dataType: "jsonp",
+		success: function(data) {
+			survey[index].questions = data;
+			console.log(data);
+			var test = true;
+			for (var i = 0; i < survey.length; i++) {
+				if (survey[i].questions === undefined) {
+					test = false;
 				}
 			}
-		});
-	};
+			if (test) {
+				constructForm();
+			}
+		}
+	});
+};
 
-	function constructForm() {
-		console.log(survey);
-		console.log(survey[1].questions);
+function constructForm() {
+	var cards = [];
+	for (var i = 0; i < survey.length; i++) {
+		cards.push(createQuestions(survey[i], i));
 	}
 	
-	var question1 = createQuestion();
-	var form =  new Ext.Carousel({
+	cards.push({html: "Survey complete"});
+	
+	console.log(cards);
+	form =  new Ext.Carousel({
       defaults: {
           cls: 'card'
       },
-		    items: [question1, {
-		        title: 'Sliders',
-		        xtype: 'form',
-		        items: [{
-		            xtype: 'fieldset',
-		            defaults: {
-		                labelAlign: 'right'
-		            },
-		            items: [{
-		                xtype: 'sliderfield',
-		                name: 'value',
-		                label: 'Value'
-		            }, {
-		                xtype: 'togglefield',
-		                name: 'enable',
-		                label: 'Enable'
-		            }]
-		        }]
-		    }]
-		});
+      items: cards
+  });
+	new Ext.Panel({
+	            fullscreen: true,
+	            
+	            layout: {
+	                type: 'vbox',
+	                align: 'stretch'
+	            },
+	            defaults: {
+	                flex: 1,
+	                id: 'education'
+	            },
+	            items: [form],
+	        });	
+}
 
-	return form;
+
+function createQuestions(group, index) {
+	var questions = [];
+	var groupQuestions = group.questions;
+	for (var question in groupQuestions) {
+		
+		questions.push(createQuestion(groupQuestions[question]));
+	}
+
+	var form = {
+		        title: group.title,
+		        xtype: 'form',
+		        id: group.id,
+		        items: questions
+		    };
+		    return form;
 };
 
-function createQuestion() {
-	var form = {
-		        title: 'Basic',
-		        xtype: 'form',
-		        id: 'basicform',
-		        items: [{
+function createQuestion(question) {
+	console.log(question);
+	var items = [];
+	for (var index in question.options) {
+		 if(question.options.hasOwnProperty(index)) {
+		var option = question.options[index];
+		items.push({name: 'answer', value: option.points, label: option.option_value});
+	}
+	};
+	return {
 		            xtype: 'fieldset',
-		            title: 'Favorite color',
+		            title: question.question,
 		            defaults: {
 		                xtype: 'radiofield',
 		                labelWidth: '35%'
 		            },
-		            items: [{
-		                name: 'color',
-		                value: 'red',
-		                label: 'Red'
-		            }, {
-		                name: 'color',
-		                label: 'Blue',
-		                value: 'blue'
-		            }, {
-		                name: 'color',
-		                label: 'Green',
-		                value: 'green'
-		            }, {
-		                name: 'color',
-		                label: 'Purple',
-		                value: 'purple'
-		            }]
-		        }]
-		    };
-		    return form;
+		            items: items
+		        };
 }
