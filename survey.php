fe8488bee62640025a291485738eae232f4907f6
Dean Chen <dean@ocirs.com>
2011-03-23 12:41:12 -0400

    Created template for survey.

--- survey.php
+++ survey.php
@@ -1,3 +1,24 @@
-<?php
-    phpinfo();
-?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Survey</title>
+		
+		<!-- Sencha Touch CSS -->
+		<link rel="stylesheet" href="sencha-touch-debug.css" type="text/css">
+	
+		<!-- Custom CSS -->
+		<link rel="stylesheet" href="css/reset.css" type="text/css">
+		<link rel="stylesheet" href="css/ventilator.css" type="text/css">
+		<link rel="stylesheet" href="css/survey.css" type="text/css">
+	
+		<!-- Sencha Touch JS -->
+		<script type="text/javascript" src="sencha-touch-debug.js"></script>
+	
+		<!-- Application JS -->
+		<script type="text/javascript" src="js/survey.js"></script>
+	</head>
+	
+	<body>
+    <? include('survey_template.php') ?>
+	</body>
+</html>
